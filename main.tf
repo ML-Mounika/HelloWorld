fc0d0b76d88c7dfcc80ea8d7f7089ae033dc3aa9
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-06-17 09:51:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08a0d1e16fc3f61ea"
   instance_type = "t2.micro"
 
   tags = {
